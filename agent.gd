8675d969a15fbd7be24197949091554804b9b192
Vojguard <jirka.kropac@gmail.com>
2024-11-20 14:05:28 +0100

    stupid agent

--- agent.gd
+++ agent.gd
@@ -10,21 +10,34 @@ var thrust = false
 # This method is called on every tick to choose an action.  See README.md
 # for a detailed description of its arguments and return value.
 func action(_walls: Array[PackedVector2Array], _gems: Array[Vector2], 
-            _polygons: Array[PackedVector2Array], _neighbors: Array[Array]):
+			_polygons: Array[PackedVector2Array], _neighbors: Array[Array]):
 
-    # This is a dummy agent that just moves around randomly.
-    # Replace this code with your actual implementation.
-    ticks += 1
-    if ticks % 30 == 0:
-        spin = Random.randi_range(-1, 1)
-        thrust = bool(Random.randi_range(0, 1))
-    
-    return [spin, thrust, false]
+	# This is a dummy agent that just moves around randomly.
+	# Replace this code with your actual implementation.
+	ticks += 1
+	var closest_gem : Vector2 = _gems[0] 
+	for gem : Vector2 in _gems:
+		if ship.position.distance_to(gem) < ship.position.distance_to(closest_gem):
+			closest_gem = gem
+	
+	if (closest_gem - (ship.position + ship.velocity)).length() > 10 :
+		thrust = 1
+	else:
+		thrust = 0
+	
+	if ship.position.angle_to_point(closest_gem) > (ship.rotation):
+		spin = 1
+	elif ship.position.angle_to_point(closest_gem) < (ship.rotation):
+		spin = -1
+	else:
+		spin = 0
+	
+	return [spin, thrust, false]
 
 # Called every time the agent has bounced off a wall.
 func bounce():
-    return
+	return
 
 # Called every time a gem has been collected.
 func gem_collected():
-    return
+	return
