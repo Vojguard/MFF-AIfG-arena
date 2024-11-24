2cd4805a4df232070a82e3d09039510252a12cf7
Vojguard <jirka.kropac@gmail.com>
2024-11-24 21:48:11 +0100

    progres angle and next

--- agent.gd
+++ agent.gd
@@ -6,14 +6,11 @@ extends Node2D
 var ticks = 0
 var spin = 0
 var thrust = false
-var _polys : Array[PackedVector2Array]
 
 # This method is called on every tick to choose an action.  See README.md
 # for a detailed description of its arguments and return value.
 func action(_walls: Array[PackedVector2Array], _gems: Array[Vector2], 
 			_polygons: Array[PackedVector2Array], _neighbors: Array[Array]):
-	
-	_polys = _polygons
 	
 	# This is a dummy agent that just moves around randomly.
 	# Replace this code with your actual implementation.
@@ -23,7 +20,7 @@ func action(_walls: Array[PackedVector2Array], _gems: Array[Vector2],
 		if ship.position.distance_to(gem) < ship.position.distance_to(closest_gem):
 			closest_gem = gem
 	
-	
+	debug_path.clear_points()
 	
 	var my_poly : int = -1
 	var gem_poly : int = -1
@@ -33,16 +30,22 @@ func action(_walls: Array[PackedVector2Array], _gems: Array[Vector2],
 		if Geometry2D.is_point_in_polygon(closest_gem, _polygons[p]):
 			gem_poly = p
 	
+	debug_path.add_point(_calculate_poly_center(_polygons[my_poly]))
+	debug_path.add_point(ship.position)
+	
 	var target_pos = Vector2.ZERO
 	if my_poly == gem_poly or _find_if_neighbors(_neighbors[my_poly], gem_poly):
 		target_pos = closest_gem
 	else:
-		target_pos = _calculate_poly_center(_polygons[_bfs(my_poly, gem_poly, _neighbors)])
-	
-	debug_path.clear_points()
-	debug_path.add_point(_calculate_poly_center(_polygons[my_poly]))
-	debug_path.add_point(ship.position)
-	debug_path.add_point(target_pos)
+		var path_to_gem := _bfs(my_poly, gem_poly, _neighbors)
+		target_pos = _calculate_poly_center(_polygons[path_to_gem[0]])
+		debug_path.add_point(target_pos)
+		if path_to_gem.size() > 1:
+			closest_gem = _calculate_poly_center(_polygons[path_to_gem[1]])
+		debug_path.add_point(closest_gem)
+		var angle : float = Vector2(ship.position - target_pos).angle_to(Vector2(closest_gem - target_pos))
+		if  angle < 2 * PI / 5 and angle > 3 * PI / 5:
+			target_pos = closest_gem
 	
 	if ship.position.angle_to_point(target_pos) > (ship.rotation + 0.1):
 		spin = 1
@@ -51,7 +54,7 @@ func action(_walls: Array[PackedVector2Array], _gems: Array[Vector2],
 	else:
 		spin = 0
 	
-	if (target_pos - (ship.position + ship.velocity)).length() * ship.ACCEL > (ship.velocity).length() :
+	if (target_pos - (ship.position + ship.velocity)).length() > (ship.velocity).length() :
 		thrust = true
 	else:
 		thrust = false
@@ -70,7 +73,7 @@ func _calculate_poly_center(polygon : PackedVector2Array) -> Vector2:
 		sum += vertex
 	return sum / polygon.size()
 
-func _bfs(your_poly : int, target_poly : int, neighbors: Array[Array]) -> int:
+func _bfs(your_poly : int, target_poly : int, neighbors: Array[Array]) -> Array[int]:
 	var queue := []
 	var visited := {}
 	var parent := {}
@@ -83,16 +86,19 @@ func _bfs(your_poly : int, target_poly : int, neighbors: Array[Array]) -> int:
 		var current = queue.pop_front()
 		
 		if current == target_poly:
+			var path : Array[int] = []
 			while parent[current] != your_poly:
 				current = parent[current]
-			return current
+				path.append(current)
+			path.reverse()
+			return path
 		
 		for neighbor in neighbors[current]:
 			if neighbor not in visited:
 				queue.append(neighbor)
 				visited[neighbor] = true
 				parent[neighbor] = current
-	return -1
+	return [-1]
 
 # Called every time the agent has bounced off a wall.
 func bounce():
